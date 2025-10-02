c44fcf587985b562c398c9dfeffc5475c3b01747
OfficialKrisHass <chrishuskywolf@gmail.com>
2025-10-02 20:43:07 +0200

    Features list

--- webapp/app/components/Features/FeatureCard.tsx
+++ webapp/app/components/Features/FeatureCard.tsx
@@ -1,13 +1,23 @@
+import Image from "next/image"
+
 import styles from "./FeatureCard.module.css"
 
-export default function FeatureCard() {
+type Props = {
+    name: string,
+    description: string,
+    thumbnail: string,
+}
+
+export default function FeatureCard({ name, description, thumbnail } : Props) {
 
     return (
         <div className={styles.card}>
-            <div className={styles.thumbnail}/>
+            <div className={styles.thumbnail}>
+                <Image src={thumbnail} alt="Feature thumbnail" fill={true}/>
+            </div>
             <div className={styles.info}>
-                <h3>Feature name</h3>
-                <p>Some info about the feature okay yeah sure great mhm okay sounds good, yeah this feature is so good and great and amazing yep</p>
+                <h3>{name}</h3>
+                <p>{description}</p>
             </div>
         </div>
     )
--- webapp/app/components/Features/FeatureList.tsx
+++ webapp/app/components/Features/FeatureList.tsx
@@ -6,11 +6,45 @@ export default function FeatureList() {
 
     return (
         <div className={styles.list}>
-            <FeatureCard/>
-            <FeatureCard/>
-            <FeatureCard/>
-            <FeatureCard/>
-            <FeatureCard/>
+            <FeatureCard
+                name="Level Editor"
+                description="Copper-Engine comes with a powerful and easy-to-use level editor, aptly named Copper-Editor. Paired with the Copper-Launcher, they
+                             form the main toolkit for creating games built on the Copper-Engine."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="C# Scripting"
+                description="By using C# you can add functionality through code to your game projects utilizing the pre-packaged scripting API. This is done by
+                             writing C# scripts and creating custom Components to define your game's functionality."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="ECS"
+                description="One of the most crucial parts of the Copper-Engine is our own, custom Entity Component System (ECS) which represents the game worlds
+                             efficiently while remaining highly customizable and easy-to-use."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="Asset system"
+                description="Copper stores all built-in or custom game assets in custom, fast, contiguous memory structures for blazing-fast access with no overhead."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="Input system"
+                description="Input is handled by a fast, state-based input system offering overhead-free key state queries and event-based notifications for input
+                             changes."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="Event system"
+                description="A simple and customizable event system is a core part of Copper-Engine, making it easy to hook into the engine's flow or define your
+                             own custom events."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="Renderer"
+                description="A very minimal but fast 3D batch renderer with basic phong lighting lets you display complex and colourful game worlds."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+            <FeatureCard
+                name="Physics"
+                description="Copper-Engine uses PhysX for a fast and optimized Rigid Body simulation. A lightweight wrapper makes PhysX easy to use within your
+                             projects."
+                thumbnail="/screenshots/CopperPromotional.png"/>
+
         </div>
     )
 
