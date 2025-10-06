49aa38d3b820c33e05bb7e08cf5b516ef593d61b
OfficialKrisHass <chrishuskywolf@gmail.com>
2025-10-06 19:10:44 +0200

    Fixed mobile version

--- webapp/app/components/Layout/Navbar.tsx
+++ webapp/app/components/Layout/Navbar.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link"
+import Image from "next/image"
+
 import styles from "./Navbar.module.css"
 
 export default function Navbar() {
 
   return (
     <header className={styles.navbar}>
-      <h1>Copper-Engine</h1>
+      <div className={styles.logo}>
+      </div>
       <Link href="/">
         <p>Home</p>
       </Link>
