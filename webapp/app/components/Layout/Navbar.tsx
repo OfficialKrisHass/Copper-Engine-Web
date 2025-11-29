32fb4bfc8540e2790c12a4e6fd0c416127d7ed9c
OfficialKrisHass <chrishuskywolf@gmail.com>
2025-11-29 12:56:24 +0100

    Dropdown navbar

--- webapp/app/components/Layout/Navbar.tsx
+++ webapp/app/components/Layout/Navbar.tsx
@@ -1,30 +1,42 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
 
 import Logo from "@/public/Logo.ico"
 
+import { useState } from "react"
+
 import styles from "./Navbar.module.css"
 
 export default function Navbar() {
 
-  return (
-    <header className={styles.navbar}>
-      <div className={styles.logo}>
-        <Image src={Logo} alt="Logo" width={20} height={20}/>
-      </div>
-      <Link href="/">
-        <p>Home</p>
-      </Link>
-      <Link href="/about?view=overview">
-        <p>About</p>
-      </Link>
-      <Link href="/blog">
-        <p>Blog</p>
-      </Link>
-      <Link href="/docs">
-        <p>Documentation</p>
-      </Link>
-    </header>
-  )
+    const [open, setOpen] = useState(false);
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Image src={Logo} alt="Logo" width={20} height={20}/>
+            </div>
+            <Link href="/">
+                <p>Home</p>
+            </Link>
+            <Link href="/about?view=overview">
+                <p>About</p>
+            </Link>
+            <Link href="/blog">
+                <p>Blog</p>
+            </Link>
+            <Link className={styles.test} href="/docs" onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+                <p>Learn</p>
+                {open && (
+                    <div className={styles.dropdown}>
+                        <Link href="/guide"><p>Guide</p></Link>
+                        <Link href="/docs"><p>Docs</p></Link>
+                    </div>
+                )}
+            </Link>
+        </nav>
+    )
 
 }
